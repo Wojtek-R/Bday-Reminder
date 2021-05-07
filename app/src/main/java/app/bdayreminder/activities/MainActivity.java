996ea471a5ba040269a1aea866222f58e2e42bb7
Wojtek-R <qdxgm4uz>
2021-05-07 17:20:52 +0100

    Changing pages on click working.

--- app/src/main/java/app/bdayreminder/activities/MainActivity.java
+++ app/src/main/java/app/bdayreminder/activities/MainActivity.java
@@ -2,16 +2,33 @@ package app.bdayreminder.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addBdayBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addBdayBtn=(Button)findViewById(R.id.main_addBtn);
+        addBdayBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(MainActivity.this, AddBday.class);
+                startActivity(intent);
+            }
+        });
     }
 
+
+
+    public void goTo(View view){
+        Button addBirthday = findViewById(R.id.main_addBtn);
+    }
 }
