7a22a685987eaf0a45eca3341223a95317b8773f
Wojtek-R <qdxgm4uz>
2021-05-07 16:26:19 +0100

    Initial commit

---
+++ app/src/main/java/app/bdayreminder/activities/AddBday.java
@@ -0,0 +1,14 @@
+package app.bdayreminder.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bday);
+    }
+}
